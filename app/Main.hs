194e148571597f33555371703dfca6ef4a2cc217
Bob Ippolito <bob@redivi.com>
2017-12-28 22:59:09 -0800

    A thing, it works

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
-import Diagrams.Backend.SVG.CmdLine (mainWith)
+import Diagrams.Prelude (Diagram, strokeLoop)
+import Diagrams.Backend.SVG.CmdLine (B, mainWith)
 import Lib (koch)
 
+example :: Diagram B
+example = strokeLoop (koch 4)
+
 main :: IO ()
-main = mainWith (koch 4)
+main = mainWith example
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,6 @@ module Lib
     ) where
 
 import Diagrams.Prelude
-import Diagrams.Backend.SVG.CmdLine
 
 koch :: Int -> Trail' Loop V2 Double
 koch n = glueLine . mconcat . iterateN 3 (rotateBy (-1/3)) $ kochSegment n
@@ -11,15 +10,12 @@ koch n = glueLine . mconcat . iterateN 3 (rotateBy (-1/3)) $ kochSegment n
 kochSegment :: Int -> Trail' Line V2 Double
 kochSegment n = case compare n 1 of
     LT -> mempty
-    EQ -> kochLine
-    GT -> seg
-       ||| (seg # rotate (-45 @@ deg) # translateY ty)
-       ||| (seg # rotate (45 @@ deg) # translateY ty)
-       ||| seg
+    EQ -> fromOffsets [unitX]
+    GT -> mconcat
+        [ seg
+        , seg # rotateBy (1/6)
+        , seg # rotateBy (-1/6)
+        , seg
+        ]
     where
-        seg = kochSegment (n - 1) # scale t
-        t = 1 / 3
-        ty = t / 2
-
-kochLine :: Trail' Line V2 Double
-kochLine = fromOffsets . map r2 $ [(1, 0)]
+        seg = kochSegment (n - 1)
