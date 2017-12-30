c05e08f382837e6c69fefc167a271c92d416c323
Bob Ippolito <bob@redivi.com>
2017-12-29 17:55:42 -0800

    Move everything to lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,7 @@
-{-# LANGUAGE FlexibleInstances         #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE TypeFamilies              #-}
-
 module Main where
 
-import Data.Colour.Palette.ColorSet
-import Diagrams.Backend.SVG.CmdLine
-import Diagrams.Prelude
-import Lib (koch)
-
-kochWidth :: Int -> Double
-kochWidth n = 3 ^ (n - 1)
-
-kochScale :: Int -> Double
-kochScale n = (0.7 ^ (n - 2)) * (1 / kochWidth n)
-
-kochLoop :: Int -> Diagram B
-kochLoop n = lc (d3Colors1 n) . scale (kochScale n) . center . strokeLoop $ koch n
-
-example :: Int -> Diagram B
-example n = mconcat (map kochLoop [2..n])
+import Diagrams.Backend.SVG.CmdLine (mainWith)
+import Lib (kochExample)
 
 main :: IO ()
-main = mainWith example
+main = mainWith kochExample
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,9 @@
 module Lib
-    ( koch
+    ( kochExample
     ) where
 
+import Data.Colour.Palette.ColorSet
+import Diagrams.Backend.SVG.CmdLine
 import Diagrams.Prelude
 
 koch :: Int -> Trail' Loop V2 Double
@@ -19,3 +21,15 @@ kochSegment n = case compare n 1 of
         ]
     where
         seg = kochSegment (n - 1)
+
+kochWidth :: Int -> Double
+kochWidth n = 3 ^ (n - 1)
+
+kochScale :: Int -> Double
+kochScale n = (0.7 ^ (n - 2)) * (1 / kochWidth n)
+
+kochLoop :: Int -> Diagram B
+kochLoop n = lc (d3Colors1 n) . scale (kochScale n) . center . strokeLoop $ koch n
+
+kochExample :: Int -> Diagram B
+kochExample n = mconcat (map kochLoop [2..n])
